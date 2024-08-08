1c07db7d5aa062e5dc9b07799b4999de33c29b8e
Matteo Piccini <gnappo.1@live.it>
2024-08-08 09:01:48 -0700

    add base code for lecture

---
+++ 05_client_server_communication/server/routes/production/productions.py
@@ -0,0 +1,24 @@
+from routes.__init__ import Resource, request, db, make_response
+from models.production import Production
+
+class Productions(Resource):
+    def get(self):
+        try:
+            serialized_prods = [prod.to_dict() for prod in Production.query]
+            # return make_response(serialized_prods, 200)
+            return serialized_prods, 200
+        except Exception as e:
+            return {"error": str(e)}
+
+    def post(self):
+        try:
+            data = (
+                request.get_json()
+            )  #! you might get a 405 if content type has not been set
+            prod = Production(**data)  #! model validations kick in at this point
+            db.session.add(prod)
+            db.session.commit()  #! database constraints kick in
+            return prod.to_dict(), 201
+        except Exception as e:
+            db.session.rollback()
+            return {"error": str(e)}, 400
